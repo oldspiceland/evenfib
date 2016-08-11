94a29e0cd1dbfe6eec91e2eb420cc0361823af48
George Spiceland <georgespiceland@gmail.com>
2016-08-11 00:34:15 -0400

    cleaned up output

--- fibgen/fibgen.go
+++ fibgen/fibgen.go
@@ -13,7 +13,7 @@ func GenerateSeq(seed1, seed2, limit int) {
 	fib := 0
 	for fib < limit {
 		fib = seed1 + seed2
-		fmt.Printf("The next number in the sequence is %v",fib)
+		fmt.Printf("The next number in the sequence is %v\n",fib)
 		seed1 = seed2
 		seed2 = fib
 	}
