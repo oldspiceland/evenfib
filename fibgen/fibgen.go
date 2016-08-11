5ce3bb53df1f0a9f8405cb72cd421ad98f8c8eb5
George Spiceland <georgespiceland@gmail.com>
2016-08-11 00:31:10 -0400

    created findNext and GenerateSeq functions in fibgen

---
+++ fibgen/fibgen.go
@@ -0,0 +1,20 @@
+package fibgen
+
+import (
+	"fmt"
+)
+
+func findNext(fib1, fib2 int) (fib3 int) {
+	fib3 := fib1 + fib2
+	return
+}
+
+func GenerateSeq(seed1, seed2, limit int) {
+	fib := 0
+	for fib < limit {
+		fib = seed1 + seed2
+		fmt.Printf("The next number in the sequence is %v",fib)
+		seed1 = seed2
+		seed2 = fib
+	}
+}
