8627f4d75db35114898494db8aa8327ebb4b835e
George Spiceland <georgespiceland@gmail.com>
2016-08-11 00:37:29 -0400

    GenerateSeq now respects the limit's authority

--- fibgen/fibgen.go
+++ fibgen/fibgen.go
@@ -13,8 +13,10 @@ func GenerateSeq(seed1, seed2, limit int) {
 	fib := 0
 	for fib < limit {
 		fib = seed1 + seed2
-		fmt.Printf("The next number in the sequence is %v\n",fib)
-		seed1 = seed2
-		seed2 = fib
+		if fib < limit {
+			fmt.Printf("The next number in the sequence is %v\n",fib)
+			seed1 = seed2
+			seed2 = fib
+		}
 	}
 }
