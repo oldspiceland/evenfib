259b2b66d8b4be029d9f1e204d9fcbf7be690925
George Spiceland <georgespiceland@gmail.com>
2016-08-11 21:10:37 -0400

    Added summing function to main.go

--- main.go
+++ main.go
@@ -5,5 +5,18 @@ import (
 )
 
 func main() {
-	fibgen.GenerateSeq(1,2,4000000)
+	seq := fibgen.GenerateSeq(1, 2, 4000000)
+
+	fibSum := summer(seq)
+	fmt.Printf("The sum of the even sequence members is %v\n", fibSum)
+}
+
+func summer(seq int) (sum int) {
+	sum := 0
+	for i = 0; i <= len(seq); i++ {
+		if seq[i]%2 == 0 {
+			sum += seq[i]
+		}
+	}
+	return
 }
