c4f0e5da4d06e4b4db6f79a349e7e47a4af4424c
George Spiceland <georgespiceland@gmail.com>
2016-08-11 21:02:26 -0400

    Created slice to be passed back out of the GenerateSeq function

--- fibgen/fibgen.go
+++ fibgen/fibgen.go
@@ -9,14 +9,17 @@ func findNext(fib1, fib2 int) (fib3 int) {
 	return
 }
 
-func GenerateSeq(seed1, seed2, limit int) {
+func GenerateSeq(seed1, seed2, limit int) (seq int) { //GenerateSeq takes two seeds and a limit
+	seq := make([]int, 10000)
 	fib := 0
 	for fib < limit {
 		fib = seed1 + seed2
 		if fib < limit {
-			fmt.Printf("The next number in the sequence is %v\n",fib)
+			fmt.Printf("The next number in the sequence is %v\n", fib)
 			seed1 = seed2
 			seed2 = fib
+			seq = append(seq, fib)
 		}
 	}
+	return
 }
